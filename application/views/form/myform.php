ffcb61fa8e9c2a119f560f0e430f2f9576b1a0c1
Marianne Rentchler <rentchlerm@eisti.eu>
2016-05-12 09:09:49 +0200

    Ajout début de formulaire

---
+++ application/views/form/myform.php
@@ -0,0 +1,12 @@
+<?php echo validation_errors(); ?>
+
+<?php echo form_open('form'); ?>
+
+
+<h5>Utilisateur</h5>
+<input type="txt" name="username" value="">
+
+<h5>Mot de passe</h5>
+<input rype="password" nam="password" value="">
+
+</form>
