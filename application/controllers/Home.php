83796ae44c5dad01cb132f65bc7a14ccade17d91
Gaël Berthaud-Müller <gael.berthaud.muller@eisti.fr>
2016-05-12 09:26:10 +0200

    [WIP] Accueil / template

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,8 +2,18 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
+
+  public function index(){
+    $data['title'] = 'Bienvenue';
+
+    $this->load->view('home/index', $data);
+  }
+  
   public function accueil(){
     $data['title'] = 'Accueil';
-    $this->load->view('home/index');
+
+    $this->load->view('templates/head', $data);
+    $this->load->view('home/accueil', $data);
+    $this->load->view('templates/foot', $data);
   }
 }
