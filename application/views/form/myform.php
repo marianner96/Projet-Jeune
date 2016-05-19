60b2ad10f041b4da73e0942f5b5e21a1f06c5246
Marianne Rentchler <rentchlerm@eisti.eu>
2016-05-19 16:52:11 +0200

    ajout de php

--- application/views/form/myform.php
+++ application/views/form/myform.php
@@ -3,23 +3,26 @@
   <div class="ui grid stackable two column middle aligned relative very relaxed">
     <?php echo form_open('connexion', array('class' => 'ui form column')); ?>
 
-
-      <div class="field">
-        <label for="email">Email</label>
-        <div class="ui left icon input">
-          <input type="email" id="email" name="email" value="<?php echo set_value('user'); ?>">
-          <i class="at icon"></i>
+      <div class="ui form error">
+        <div class="field">
+          <label for="email">Email</label>
+          <div class="ui left icon input">
+            <input type="email" id="email" name="email" value="<?php echo set_value('user'); ?>">
+            <i class="at icon"></i>
+          </div>
+          <?php if (form_error('email') != "") {echo "<div class='ui error message'>";echo form_error('email'); echo "</div>";} ?>
         </div>
-        <?php if (form_error('email') != "") {echo "<div class='alert alert-warning'>";echo form_error('email'); echo "</div>";} ?>
       </div>
 
-      <div class="field">
-        <label for="pass">Mot de passe</label>
-        <div class="ui left icon input">
-          <input type="password" id="pass" name="pass" value="<?php echo set_value('pass'); ?>"> 
-          <i class="lock icon"></i>
+      <div class="ui form error">
+        <div class="field">
+          <label for="pass">Mot de passe</label>
+          <div class="ui left icon input">
+            <input type="password" id="pass" name="pass" value="<?php echo set_value('pass'); ?>"> 
+            <i class="lock icon"></i>
+          </div>
+          <?php if (form_error('pass') != "") {echo "<div class='ui error message'>";echo form_error('pass'); echo "</div>";} ?>
         </div>
-        <?php if (form_error('user') != "") {echo "<div class='alert alert-warning'>";echo form_error('pass'); echo "</div>";} ?>
       </div>
 
       <input class="ui button" type="submit" value="Connexion">
