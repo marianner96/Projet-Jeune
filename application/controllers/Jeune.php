51ae3ade298932a6086a1540d399257715fa130a
warnier de wailly clément <clement.warnier.de.wailly@eisti.fr>
2016-05-24 15:32:05 +0200

    WIP verification du formulaire jeune

--- application/controllers/Jeune.php
+++ application/controllers/Jeune.php
@@ -8,10 +8,18 @@ class Jeune extends CI_Controller{
         $this->load->view('templates/foot');
 	}
     public function formulaire(){
-        $this->load->model('jeune_model');
-        $data['query'] = $this->jeune_model->savoiretre();
+        $this->load->model('savoiretre_model');
         $this->load->helper(array('form', 'url'));
+
         $this->load->library('form_validation');
+        $this->form_validation->set_rules('savoirEtre[]','SavoirEtre','required|max_length[2]');
+        $this->form_validation->set_rules('description', 'Description', 'required');
+        $this->form_validation->set_rules('duree', 'Durée', 'required');
+        $this->form_validation->set_rules('prenom', 'Prénom', 'required');
+        $this->form_validation->set_rules('nom', 'Nom', 'required');
+        $this->form_validation->set_rules('mail', 'Mail', 'valid_email');
+
+        $data['query'] = $this->savoiretre_model->getJeune();
         $data['content'] = 'fomulaire';
         $data['menu'] = 'jeune';
         if ($this->form_validation->run() == FALSE){
--- application/views/PartieJeune/fomulaire.php
+++ application/views/PartieJeune/fomulaire.php
@@ -1,14 +1,15 @@
 Cette référence sera envoyé par mail à votre référent qui pourra validé votre demande et selectionner les savoir-être vous correspondant.
 <div class="ui grid">
 <div class="ten wide column">
-<form class="ui small form" >
+ <?php
+      echo form_open('jeune/formulaire', array('class' => 'ui small form')); ?>
    <h4 class="ui dividing header">Savoir-être</h4>
    <div class="field">
-    <select multiple="" class="ui dropdown" name="savoirEtre">
+    <select multiple="" class="ui dropdown" name="savoirEtre[]" data-validate="savoirEtre">
       <option value="">Selectionner vos savoir être</option>
       <?php
 foreach ($query as $value) {
-    echo "<option>" . $value->nom . "</option>";
+    echo "<option value=\"$value->id\" " . set_select('savoirEtre', $value->id) . ">" . $value->nom . "</option>";
 }
 ?>
     </select>
@@ -17,11 +18,12 @@ foreach ($query as $value) {
   <div class="two fields">
     <div class="field">
       <label>Description de l'engagement</label>
-      <textarea rows="2" name="description"></textarea>
+       <?php echo form_error('description')?>
+      <textarea rows="2" name="description"><?php echo set_value('description'); ?></textarea>
     </div>
     <div class="field">
       <label>Durée de l'engagement</label>
-      <input type="text" name="duree">
+      <input type="text" name="duree" value="<?php echo set_value('duree'); ?>">
     </div>
   </div>
   <h4 class="ui dividing header">Référent</h4>
@@ -29,19 +31,21 @@ foreach ($query as $value) {
       <label>Identité</label>
         <div class="two fields">
           <div class="field">
-            <input type="text" name="prenom" placeholder="Prénom">
+            <input type="text" name="prenom" placeholder="Prénom" value="<?php echo set_value('prenom'); ?>">
           </div>
           <div class="field">
-            <input type="text" name="nom" placeholder="Nom">
+            <input type="text" name="nom" placeholder="Nom" value="<?php echo set_value('nom'); ?>">
           </div>
         </div>
     </div>
   <div class="field">
     <label>Adresse email</label>
-      <input placeholder="E-mail" type="email" name="mail">
+    <?php echo form_error('mail')?>
+      <input placeholder="E-mail" type="email" name="mail" value="<?php echo set_value('mail'); ?>">
   </div>
    <div class="ui submit button">Envoyer</div>
   <div class="ui error message"></div>
+
 </form>
 </div>
   <div class="six wide column"></div> 
@@ -50,67 +54,67 @@ foreach ($query as $value) {
 $('select.dropdown')
   .dropdown()
 ;
-$('.ui.form')
-  .form({
-    fields: {
-      savoiretre :{
-        identifier :'savoirEtre',
-        rules: [
-          {
-            type   : 'maxCount[4]',
-            prompt : 'Veuillez selectionner au maximum 4 savoir-être'
-          },
-          {
-            type   : 'minCount[1]',
-            prompt : 'Veuillez selectionner au moins un savoir-être'
-          }
-        ]
-      },
-    description:{
-      identifier :'description',
-        rules: [
-          {
-            type   : 'empty',
-            prompt : 'Veuillez mettre une description'
-          }
-        ]
-    },
-    duree:{
-      identifier :'duree',
-        rules: [
-          {
-            type   : 'empty',
-            prompt : 'Veuillez préciser la durée de votre engagement'
-          }
-        ]
-    },
-    prenom:{
-      identifier :'prenom',
-        rules: [
-          {
-            type   : 'empty',
-            prompt : 'Veuillez préciser le prenom du référent'
-          }
-        ]
-    },
-      nom:{
-      identifier :'nom',
-        rules: [
-          {
-            type   : 'empty',
-            prompt : 'Veuillez préciser le nom du référent'
-          }
-        ]
-    },
-  mail:{
-      identifier :'mail',
-        rules: [
-          {
-            type   : 'empty',
-            prompt : 'Veuillez préciser le mail du référent'
-          }
-        ]
-    },
-  }
-})
+// $('.ui.form')
+//   .form({
+//     fields: {
+//       savoiretre :{
+//         identifier :'savoirEtre',
+//         rules: [
+//           {
+//             type   : 'maxCount[4]',
+//             prompt : 'Veuillez selectionner au maximum 4 savoir-être'
+//           },
+//           {
+//             type   : 'minCount[1]',
+//             prompt : 'Veuillez selectionner au moins un savoir-être'
+//           }
+//         ]
+//       },
+//     description:{
+//       identifier :'description',
+//         rules: [
+//           {
+//             type   : 'empty',
+//             prompt : 'Veuillez mettre une description'
+//           }
+//         ]
+//     },
+//     duree:{
+//       identifier :'duree',
+//         rules: [
+//           {
+//             type   : 'empty',
+//             prompt : 'Veuillez préciser la durée de votre engagement'
+//           }
+//         ]
+//     },
+//     prenom:{
+//       identifier :'prenom',
+//         rules: [
+//           {
+//             type   : 'empty',
+//             prompt : 'Veuillez préciser le prenom du référent'
+//           }
+//         ]
+//     },
+//       nom:{
+//       identifier :'nom',
+//         rules: [
+//           {
+//             type   : 'empty',
+//             prompt : 'Veuillez préciser le nom du référent'
+//           }
+//         ]
+//     },
+//   mail:{
+//       identifier :'mail',
+//         rules: [
+//           {
+//             type   : 'empty',
+//             prompt : 'Veuillez préciser le mail du référent'
+//           }
+//         ]
+//     },
+//   }
+// })
 </script>
