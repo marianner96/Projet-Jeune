308277fe9a02026adbb9b1fb65d8d6cb32e12b6a
Marianne Rentchler <rentchlerm@eisti.eu>
2016-05-25 13:49:06 +0200

    un truc a change mais je sais plus quoi

--- application/controllers/Jeune.php
+++ application/controllers/Jeune.php
@@ -1,5 +1,11 @@
 <?php
 class Jeune extends CI_Controller{
+    public function __construct(){
+          parent::__construct();
+          $this->load->library("form_validation");
+          $this->load->library('session');
+        }
+
 	public function index(){
         $data['content'] = 'accueil';
         $data['menu'] = 'jeune';
@@ -11,7 +17,6 @@ class Jeune extends CI_Controller{
         $this->load->model('jeune_model');
         $data['query'] = $this->jeune_model->savoiretre();
         $this->load->helper(array('form', 'url'));
-        $this->load->library('form_validation');
         $data['content'] = 'fomulaire';
         $data['menu'] = 'jeune';
         if ($this->form_validation->run() == FALSE){
@@ -33,6 +38,15 @@ class Jeune extends CI_Controller{
     public function profil(){
         $data['content'] = 'profil';
         $data['menu'] = 'jeune';
+        $tab = $this->session->userdata('logged_in'); 
+        $data['tab'] = $tab;
+
+        $this->form_validation->set_rules('nom', 'nom', 'required');
+        $this->form_validation->set_rules('prenom', "prénom", 'required');
+        $this->form_validation->set_rules('date_naissance', 'date de naissance', 'required');
+        $this->form_validation->set_rules('mail', 'e-mail', 'required|valid_email');
+        $this->form_validation->set_rules('mdp', 'mot de passe', 'required');
+
         $this->load->view('templates/head', $data);
         $this->load->view('templates/jeunes', $data);
         $this->load->view('templates/foot');
