c7b533238de9a962c1072f368d6f7ed9d838d1b7
Gaël Berthaud-Müller <gael.berthaud.muller@eisti.fr>
2016-06-08 22:46:10 +0200

    Ajout lien partie admin

--- application/views/templates/admin.php
+++ application/views/templates/admin.php
@@ -2,6 +2,7 @@
   <div class="ui grid stackable">
     <div class="four wide column">
       <div class="ui vertical pointing menu">
+        <a class="item <?php echo $content == 'index' ? 'active' : ''?>" href="<?php echo site_url('admin') ?>">Vue d'ensemble</a>
         <a class="item <?php echo $content == 'savoir_etre' ? 'active' : ''?>" href="<?php echo site_url('admin/savoir-etre') ?>">Savoir-être</a>
         <a class="item <?php echo $content == 'users' ? 'active' : ''?>" href="<?php echo site_url('admin/utilisateurs') ?>">Membres</a>
       </div>
